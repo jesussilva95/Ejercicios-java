4df864b59b17d86643530045a425aef0a5b6052c
Jesus silva <100956379+jesussilva95@users.noreply.github.com>
2025-02-22 22:24:25 -0500

    ejercicio cuatro teminado

---
+++ ejercicios-Java-uno/src/javaEjercicios/Ejer4.java
@@ -0,0 +1,30 @@
+package javaEjercicios;
+
+import java.util.Scanner;
+
+public class Ejer4 {
+	/*
+	* Escribe un programa que simule un sensor de temperatura ambiente y un umbral de alerta. El
+	* usuario ingresa la temperatura actual, y el programa verifica si la temperatura está por encima
+	* del umbral de alerta. Si es así, muestra un mensaje de alerta. Si no, muestra un mensaje de
+	* que la temperatura es normal.
+	* */
+	private Scanner lectura;
+	public Ejer4(Scanner lectura){
+		this.lectura = lectura;
+	}
+
+	public float tomarTemperatura() {
+		System.out.println("Escriba la temperatura actual: ");
+		float tempe = lectura.nextFloat();
+		return tempe;
+	}
+
+	public void calcularTemperatura(float tempe) {
+		if (tempe < 30){
+			System.out.println("Temperatura esta en rango: ");
+		}else {
+			System.out.println("Esta demasiada alta la temperatura toma una ceverza ");
+		}
+	}
+}
--- ejercicios-Java-uno/src/javaEjercicios/Principal.java
+++ ejercicios-Java-uno/src/javaEjercicios/Principal.java
@@ -6,22 +6,23 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner lectura = new Scanner(System.in);
 
-
-
+		Ejer4 ejer4 = new Ejer4(lectura);
+		float tempe = ejer4.tomarTemperatura();
+		ejer4.calcularTemperatura(tempe);
 		/*
-
-		Ejercicio 3
-		Ejer3 ejer3 = new Ejer3(lectura);
-		ejer3.tomarTemperatura();
+		Ejercicio 1
+		Ejer1 ejer1 = new Ejer1(lectura);
+		ejer1.TomarPiso();
 
 		Ejercicio 2
 		Ejer2 ejer2 = new Ejer2(lectura);
 		ejer2.tomarPeso();
 
-		Ejercicio 1
-		Ejer1 ejer1 = new Ejer1(lectura);
-		ejer1.TomarPiso();
+		Ejercicio 3
+		Ejer3 ejer3 = new Ejer3(lectura);
+		ejer3.tomarTemperatura();
 
 		 */
 	}
 }
+
